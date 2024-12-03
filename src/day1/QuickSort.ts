12bde09712db7d64b94a1cec0fbd2d76f504dad5
MoHashem24 <MohammadHashemHashem24@gmail.comcls>
2024-12-03 07:57:27 +0200

    add quick_sort

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,26 @@
+ function qs(arr: number[], low: number, high: number): void {
+ if(low >= high) return;
+ let pivotIndex = parttion(arr, low, high);
+ //pivotIndex is excluded as already done in parttion
+ qs(arr, low,pivotIndex-1);
+ qs(arr, pivotIndex+1, high);
+}
+function parttion(arr: number[], low: number, high: number): number {
+    let pivot = arr[high];
+    let idx = low -1;
+    for(let i = low; i < high; i++){
+       
+        //swap if i < pivot
+        if(arr[i] <= pivot){
+            idx++;// move pivot index if swaapped already and smaller 
+            [arr[idx], arr[i]] = [arr[i], arr[idx]];
+        }
+    }
+    //swap pivot with idx
+    ++idx;
+    [arr[idx], arr[high]] = [arr[high], arr[idx]];
+    return idx;
+}
 export default function quick_sort(arr: number[]): void {
-
+    qs(arr, 0, arr.length-1);
 }
