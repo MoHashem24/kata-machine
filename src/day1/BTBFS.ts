19f1c861aaf2b5777d458db8b0bb3c0bf9c109ea
MoHashem24 <MohammadHashemHashem24@gmail.comcls>
2024-12-13 20:31:44 +0200

    add bfs

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,11 @@
-export default function bfs(head: BinaryNode<number>, needle: number): boolean {
 
+export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    let queue: BinaryNode<number>[] = [head];
+    while(!!queue.length){
+        const node = queue.shift();
+        if (node?.value === needle) return true;
+        node?.left && queue.push(node.left);
+        node?.right && queue.push(node.right);
+    }  
+    return false;      
 }
