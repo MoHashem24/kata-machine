87efa13bcac749d5763130ba1e675f0a1507fd75
MoHashem24 <MohammadHashemHashem24@gmail.comcls>
2024-12-13 20:38:12 +0200

    implement depth-first traversal for binary tree comparison

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,12 @@
+//need DFs traversal to compare as DFs keep same shape
 export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+    if (a === null && b === null) return true;
+    if (a === null && b !== null) return false;
+    if (a !== null && b === null) return false;
 
+    if (a?.value !== b?.value) return false;
+    if (!compare(a?.left || null, b?.left || null)) return false;
+    if (!compare(a?.right || null, b?.right || null)) return false;
+
+    return true;
 }
