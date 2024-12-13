ed64b79095a53c48b5cd5e46c29ed64538557a2a
MoHashem24 <MohammadHashemHashem24@gmail.comcls>
2024-12-13 20:14:32 +0200

    build dfs

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,13 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+function walk(head:BinaryNode<number>,needle:number):boolean{
+    if(head === null) return false;
+    if(head.value === needle) return true;
+    
+    if(head.left && walk(head.left,needle)) return true;
+    if(head.right && walk(head.right,needle)) return true;
 
+
+    return false;
+}
+export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+    return walk(head,needle)
 }
