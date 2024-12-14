e378ca3ad1942a34882d2f73c702199c05bd4b29
MoHashem24 <MohammadHashemHashem24@gmail.comcls>
2024-12-14 14:14:59 +0200

    refactor walk function for optimized binary search traversal

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -2,11 +2,11 @@ function walk(head:BinaryNode<number>,needle:number):boolean{
     if(head === null) return false;
     if(head.value === needle) return true;
     
-    if(head.left && walk(head.left,needle)) return true;
-    if(head.right && walk(head.right,needle)) return true;
+    if(needle > head.value){ 
+        return !!head.right && walk(head.right,needle)
+    }
 
-
-    return false;
+    return !!head.left && walk(head.left,needle);
 }
 export default function dfs(head: BinaryNode<number>, needle: number): boolean {
     return walk(head,needle)
